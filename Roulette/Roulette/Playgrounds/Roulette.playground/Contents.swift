d738ba23db05568ebe55c0d038e6e67f7c974388
Thomas Sivilay <thomas.sivilay@gmail.com>
2017-07-04 12:09:51 +0200

    Moved code from playground to UIWheelFramewok

--- Roulette/Roulette/Playgrounds/Roulette.playground/Contents.swift
+++ Roulette/Roulette/Playgrounds/Roulette.playground/Contents.swift
@@ -1,70 +1,22 @@
 import UIKit
 import PlaygroundSupport
+import UIWheel
 
 let view = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
 view.backgroundColor = .white
 
-let wheelBackground = UIView(frame: CGRect(x: 20, y: 20, width: 460, height: 460))
-view.addSubview(wheelBackground)
-wheelBackground.backgroundColor = .red
-wheelBackground.clipsToBounds = true
-wheelBackground.layer.cornerRadius = 460 / 2
-
-let choices = [1,2,3,4,5]
-let nbChoices = choices.count
-
-let r: Double = 460
-let deuxpiR = r * Double.pi
-let middleCoordinate = CGPoint(x: wheelBackground.frame.width / 2, y: wheelBackground.frame.height / 2)
-
-var coordinates: [CGPoint] = []
-
-for i in 0..<nbChoices {
-    print(i)
-    let angle: Double = Double(i) * Double(360) / Double(nbChoices)
-    print (angle)
-    let radAngle = angle * M_PI / 180.0
-    var point = CGPoint.zero
-    point.x = CGFloat(Double(middleCoordinate.x) + r * cos(radAngle))
-    point.y = CGFloat(Double(middleCoordinate.y) + r * sin(radAngle))
-    coordinates.append(point)
-    print(point)
-}
-
-var colors: [UIColor] = [.yellow, .blue, .green, .orange, .purple]
-
-let ctx = UIGraphicsGetCurrentContext();
-var startAngle = coordinates.first!
-var i = 0
-for coordinate in coordinates {
-    let shape = CAShapeLayer()
-    wheelBackground.layer.addSublayer(shape)
-    shape.opacity = 1
-    shape.lineWidth = 2
-    shape.lineJoin = kCALineJoinMiter
-    shape.fillColor = colors[i].cgColor
-    
-    let path = UIBezierPath()
-    path.move(to: startAngle)
-    path.addLine(to: coordinate)
-    path.addLine(to: middleCoordinate)
-    path.close()
-    
-    shape.path = path.cgPath
-    i = i + 1
-    startAngle = coordinate
-}
+let wheelView = UIWheel(frame: CGRect(x: 20, y: 20, width: 460, height: 460))
+wheelView.backgroundColor = .black
+view.addSubview(wheelView)
 
 let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
 rotationAnimation.toValue = NSNumber(value: Double.pi * 2 * 10)
-rotationAnimation.duration = 10
+rotationAnimation.duration = 5
 rotationAnimation.isCumulative = true
 rotationAnimation.repeatCount = 1
 rotationAnimation.timingFunction =
     CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-wheelBackground.layer.add(rotationAnimation, forKey: "rotationAnimation")
+wheelView.layer.add(rotationAnimation, forKey: "rotationAnimation")
 
 PlaygroundPage.current.liveView = view
 PlaygroundPage.current.needsIndefiniteExecution = true
-
-
--- Roulette/UIWheel/Sources/UIWheel.swift
+++ Roulette/UIWheel/Sources/UIWheel.swift
@@ -12,13 +12,19 @@ public final class UIWheel: UIView {
     
     // MARK: - Properties
     
-    public var choices: [String]
+    public var choices: [String] {
+        didSet {
+            // draw
+        }
+    }
     
     // MARK: - Initializers
     
     public override init(frame: CGRect) {
-        self.choices = []
+        self.choices = ["1", "2", "3", "4"]
         super.init(frame: frame)
+        setUp()
+        drawSliceSublayers()
     }
     
     public required init?(coder aDecoder: NSCoder) {
@@ -29,4 +35,45 @@ public final class UIWheel: UIView {
     
     // MARK: - Methods
     
+    private func setUp() {
+        clipsToBounds = true
+        layer.cornerRadius = frame.width / 2
+    }
+    
+    private func drawSliceSublayers() {
+        let r: Double = Double(frame.width)
+        let center = CGPoint(x: frame.width / 2, y: frame.height / 2)
+        var coordinates: [CGPoint] = []
+        var colors: [UIColor] = [.yellow, .blue, .green, .orange, .purple]
+        
+        for i in 0..<choices.count {
+            let angle: Double = Double(i) * Double(360) / Double(choices.count)
+            let radAngle = angle * .pi / 180.0
+            var point = CGPoint.zero
+            point.x = CGFloat(Double(center.x) + r * cos(radAngle))
+            point.y = CGFloat(Double(center.y) + r * sin(radAngle))
+            coordinates.append(point)
+        }
+        
+        var startAngle = coordinates.first!
+        var i = 0
+        for coordinate in coordinates {
+            let shape = CAShapeLayer()
+            layer.addSublayer(shape)
+            shape.opacity = 1
+            shape.lineWidth = 2
+            shape.lineJoin = kCALineJoinMiter
+            shape.fillColor = colors[i].cgColor
+            
+            let path = UIBezierPath()
+            path.move(to: startAngle)
+            path.addLine(to: coordinate)
+            path.addLine(to: center)
+            path.close()
+            
+            shape.path = path.cgPath
+            i = i + 1
+            startAngle = coordinate
+        }
+    }
 }
