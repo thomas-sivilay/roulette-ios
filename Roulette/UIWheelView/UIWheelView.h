0d8fd99946d7eb5c15b8b568a59b985fa3d51447
Thomas Sivilay <thomas.sivilay@gmail.com>
2017-07-07 16:17:28 +0200

    Added UIWheelView

---
+++ Roulette/UIWheelView/UIWheelView.h
@@ -0,0 +1,19 @@
+//
+//  UIWheelView.h
+//  UIWheelView
+//
+//  Created by Thomas Sivilay on 7/7/17.
+//  Copyright © 2017 Thomas Sivilay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIWheelView.
+FOUNDATION_EXPORT double UIWheelViewVersionNumber;
+
+//! Project version string for UIWheelView.
+FOUNDATION_EXPORT const unsigned char UIWheelViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIWheelView/PublicHeader.h>
+
+
